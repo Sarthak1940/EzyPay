9f0a552bee142d6680644bef26f87544d763124e
Sarthak Godse <sarthak123godse@gmail.com>
2024-07-24 10:53:13 +0530

    fix transfer money

--- user-app/components/TransferMoney.tsx
+++ user-app/components/TransferMoney.tsx
@@ -11,17 +11,6 @@ export const TransferMoney = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [loading, setLoading] = useState(false)
 
-    async function handleMoney() {
-        try {
-            setLoading(true)
-            await initiateTransfer(amount * 100, phoneNumber)   
-            window.location.reload(); 
-            setLoading(false)
-        } catch (error) {
-            console.log("Error creating transaction", error);           
-        }    
-    }
-
     
   return <Card title="Send Money" className="max-h-fit">
   <div className="w-full">
@@ -35,7 +24,12 @@ export const TransferMoney = () => {
       }} />
 
       <div className="flex justify-center pt-4">  
-            <Button onClick={() => {handleMoney}} disabled={loading} colour="bg-[#855bfb29] text-[#7132f5]">
+            <Button onClick={async () => {
+              setLoading(true)
+              await initiateTransfer(amount * 100, phoneNumber)   
+              window.location.reload(); 
+              setLoading(false)
+            }} disabled={loading} colour="bg-[#855bfb29] text-[#7132f5]">
             Send Money
             </Button>
         </div>
